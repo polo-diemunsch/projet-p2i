0114511c29d169f2b823b8a7dca0b7b66e991626
armandp2t <armand.petit.21.05@gmail.com>
2023-05-31 10:25:50 +0200

    commandes

---
+++ R/r.py
@@ -0,0 +1,10 @@
+import rpy2
+import rpy2.robjects as robjects
+
+## To aid in printing HTML in notebooks
+import rpy2.ipython.html
+rpy2.ipython.html.init_printing()
+
+## To see plots in an output cell
+from rpy2.ipython.ggplot import image_png
+
