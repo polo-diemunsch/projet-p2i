ef34eb5ce4ad20dea4efb5f36bea6484732b3ba4
arnov <arno.venaille@gmail.com>
2023-06-05 16:58:40 +0200

    Tableau perf

--- data/Visualisation.py
+++ data/Visualisation.py
@@ -10,18 +10,18 @@ import tkinter as tk
      niv_estime = data[7]
      fenetre = tk.Frame(root)
      fenetre.pack()
-     fauxlabel = tk.Label(fenetre,text = f"Nombre de fausses notes \n{nb_fausses_notes}",bg = "#E72E20")
-     fauxlabel.grid(row = 0,column = 0,ipadx=106)
-     preclabel = tk.Label(fenetre,text = f"Précision \n {precision*100} %",bg = "#FF9900")
-     preclabel.grid(row = 0,column = 1,ipadx=150)
-     bpmlabel = tk.Label(fenetre,text = f"BPM Moyen \n {BPM_moyen} BPM",bg = "#EFF828")
+     fauxlabel = tk.Label(fenetre,text = f"Nombre de fausses notes \n{nb_fausses_notes}",bg = "#E72E20", font = "Arial 15")
+     fauxlabel.grid(row = 0,column = 0,ipadx=74)
+     preclabel = tk.Label(fenetre,text = f"Précision \n {precision*100} %",bg = "#FF9900", font = "Arial 15")
+     preclabel.grid(row = 0,column = 1,ipadx=172)
+     bpmlabel = tk.Label(fenetre,text = f"BPM Moyen \n {BPM_moyen} BPM",bg = "#EFF828", font = "Arial 15")
      bpmlabel.grid(row = 1, column = 0,ipadx = 150)
      if ancien_niv < niv_estime:
-            nivlabel = tk.Label(fenetre,text = f"Evolution du niveau \n {ancien_niv} ↗ {niv_estime}",bg = "#50FF00")
+            nivlabel = tk.Label(fenetre,text = f"Evolution du niveau \n {ancien_niv} ↗ {niv_estime}",bg = "#50FF00", font = "Arial 15")
             nivlabel.grid(row=1,column=1,ipadx=115)
      elif ancien_niv == niv_estime:
-            nivlabel = tk.Label(fenetre,text = f"Evolution du niveau \n {ancien_niv} = {niv_estime}",bg = "#FFFFFF")
+            nivlabel = tk.Label(fenetre,text = f"Evolution du niveau \n {ancien_niv} = {niv_estime}",bg = "#FFFFFF", font = "Arial 15")
             nivlabel.grid(row=1,column=1,ipadx=115)
      else:
-            nivlabel = tk.Label(fenetre,text = f"Evolution du niveau \n {ancien_niv} ↘ {niv_estime}",bg = "#DA6262")
+            nivlabel = tk.Label(fenetre,text = f"Evolution du niveau \n {ancien_niv} ↘ {niv_estime}",bg = "#DA6262", font = "Arial 15")
             nivlabel.grid(row=1,column=1,ipadx=115)
